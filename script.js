e0618f906933022aa2aed0f9b71d15e0b60bfe94
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-12-11 15:10:58 -0800

    solved the display issue in the javascript code.

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ const body = document.querySelector('body')
 body.style.backgroundImage = 'linear-gradient(red, blue)'; 
 body.style.backgroundRepeat = 'no-repeat';
 
-function RNG(max_number) {
-    return Math.round(Math.random() * max_number);
+function RNG() {
+    return Math.round(Math.random());
 }
 
 let button = document.querySelector("button");
 let answer = document.querySelector("#answer");
 
-button.addEventListener(click, function(event) {
-    alert("clicked!")
+button.addEventListener("click", function() {
+
+    let randomNumber = 3;
+
+    let answerText = "";
     //proceedure goes here
     //0-> "yeah"
+    if (randomNumber == 0) {
+        answerText = "AFIRMATIVE!!"
+    }
     //1-> nah
+    else if (randomNumber == 1) {
+        answerText = "down and give me twenty!!"
+    }
     //2-> "try again"
+    else if (randomNumber == 2) {
+        answerText = "Last one alive lock the door!!"
+    }
     //3-> "doomkoft!"
-    let randomNumber = RNG(3);
-    aleart(randomNumber)
+    else     if (randomNumber == 3) {
+        answerText = "SPY!!!"
+    }
+    answer.innerHTML = answerText;
+
+    console.log(answerText)
     //generate a rando number from the question.
 })
