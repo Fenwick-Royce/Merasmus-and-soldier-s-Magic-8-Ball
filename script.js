7a433c485143c4383c37c44d7eb27786cf65b7e7
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-12-11 14:41:32 -0800

    javascript is semi complete. basic code is written. looking to add further additions

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const body = document.querySelector('body')
-body.style.lineargradient = 'gradiant blue red';
+body.style.backgroundImage = 'linear-gradient(red, blue)'; 
+body.style.backgroundRepeat = 'no-repeat';
+
+function RNG(max_number) {
+    return Math.round(Math.random() * max_number);
+}
+
+let button = document.querySelector("button");
+let answer = document.querySelector("#answer");
+
+button.addEventListener(click, function(event) {
+    alert("clicked!")
+    //proceedure goes here
+    //0-> "yeah"
+    //1-> nah
+    //2-> "try again"
+    //3-> "doomkoft!"
+    let randomNumber = RNG(3);
+    aleart(randomNumber)
+    //generate a rando number from the question.
+})
